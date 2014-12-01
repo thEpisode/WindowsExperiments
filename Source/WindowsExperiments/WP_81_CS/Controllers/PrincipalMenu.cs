5998afcd0f2dcb4b2ad09980eee9f0e6650b6456
Cami Rodriguez <camiepisode@outlook.com>
2014-12-01 14:58:14 -0500

    Added 4 scenario for WebView control, DefaultBackgroundColor, DOMContentLoaded

--- Source/WindowsExperiments/WP_81_CS/Controllers/PrincipalMenu.cs
+++ Source/WindowsExperiments/WP_81_CS/Controllers/PrincipalMenu.cs
@@ -40,7 +40,7 @@ namespace WP_81_CS.Controllers
             switch (selectedItem.Id)
             {
                 case 1:
-                    return typeof(WP_81_CS.Experiments.WebView.WebView3);
+                    return typeof(WP_81_CS.Experiments.WebView.WebView4);
                 default:
                     return null;
             }
---
+++ Source/WindowsExperiments/WP_81_CS/Experiments/WebView/WebView4.xaml.cs
@@ -0,0 +1,145 @@
+﻿using WP_81_CS.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace WP_81_CS.Experiments.WebView
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WebView4 : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public WebView4()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+
+            this.Loaded += WebView4_Loaded;
+        }
+
+        private void WebView4_Loaded(object sender, RoutedEventArgs e)
+        {
+            myWebView.DefaultBackgroundColor = Color.FromArgb(255, 0, 0, 0);
+
+            myWebView.NavigationStarting += MyWebView_NavigationStarting;
+            myWebView.DOMContentLoaded += MyWebView_DOMContentLoaded;
+
+            string url = "ms-appx-web:///Experiments/WebView/www/interface.html";
+            myWebView.Navigate(new Uri(url));
+        }
+
+        private void MyWebView_NavigationStarting(Windows.UI.Xaml.Controls.WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            myProgress.Visibility = Visibility.Visible;
+            myWebView.Visibility = Visibility.Collapsed;
+        }
+
+        private void MyWebView_DOMContentLoaded(Windows.UI.Xaml.Controls.WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            myProgress.Visibility = Visibility.Collapsed;
+            myWebView.Visibility = Visibility.Visible;
+            Change2ClearColor();
+        }
+
+        private async void Change2ClearColor()
+        {
+            await myWebView.InvokeScriptAsync("change2ClearColor", null);
+        }
+
+
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
